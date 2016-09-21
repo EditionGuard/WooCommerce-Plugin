ebc71e615e38bf1c62ba303494bb6a73798abca4
vlad0209 <vladislav.homenko@gmail.com>
2016-09-21 17:17:03 +0300

    API class which retrieve API token

---
+++ woo_eg_api.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * Copyright 
+ * Vladislav Khomenko
+ */
+
+/**
+ * API Interface for editionguard service
+ *
+ * @author vlad
+ */
+class Woo_eg_api {
+
+    public $token;
+
+    public function __construct($email, $password) {
+        $curl = curl_init();
+        
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://staging.editionguard.com/api/v2/obtain-auth-token",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+            CURLOPT_POSTFIELDS => array('email' => $email, 'password' => $password),
+        ));
+
+        $response = json_decode(curl_exec($curl));
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            echo "cURL Error #:" . $err;
+        } else {
+            $this->token = $response->token;
+        }
+    }
+
+}
